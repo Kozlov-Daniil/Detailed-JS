69ec06f41f28bf3e07956772a6122842a7e5ae9a
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2023-01-27 19:18:14 +0300

    Third lesson, what is a 'closures'

--- index.js
+++ index.js
@@ -26,56 +26,110 @@
 
 // const str = new String ('I am string')
 
-function hello(){
-    console.log('Hello', this)
-}
-
-const person = {
-         name: 'Eagle',
-         age: 20,
-         sayHello: hello,
-         sayHelloWindow: hello.bind(window), // = hello.bind(this)
-         logInfo: function(job, phone){
-            console.group(`${this.name} info:`)
-            console.log(`Name is ${this.name}`)
-            console.log(`Age is ${this.age}`)
-            console.log(`Job is ${job}`)
-            console.log(`Phone is ${phone}`)
-            console.groupEnd()
-         }
-     }
-
-const Danya = {
-    name: 'Daniil',
-    age: 21
-}
-
-person.logInfo()
-// person.logInfo.bind(Danya)('Programmist', '123')
-const fnDanyaInfoLog = person.logInfo.bind(Danya) //, 'Web-designer', '+79027******'\
-fnDanyaInfoLog('Web-designer', '+79027******')
-
-person.logInfo.call(Danya, 'Web-designer', '+79027******')
-person.logInfo.apply(Danya, ['Web-designer', '+79027******']) //Можно передавать только 2 параметра
-
-//==========
-
-const array = [1, 2, 3, 4, 5]
-
-// function multBy(arr, n) {
-// return arr.map(function(i){
-//     return i * n
-// })
+// function hello(){
+//     console.log('Hello', this)
 // }
 
-// console.log(multBy(array, 15))
+// const person = {
+//          name: 'Eagle',
+//          age: 20,
+//          sayHello: hello,
+//          sayHelloWindow: hello.bind(window), // = hello.bind(this)
+//          logInfo: function(job, phone){
+//             console.group(`${this.name} info:`)
+//             console.log(`Name is ${this.name}`)
+//             console.log(`Age is ${this.age}`)
+//             console.log(`Job is ${job}`)
+//             console.log(`Phone is ${phone}`)
+//             console.groupEnd()
+//          }
+//      }
+
+// const Danya = {
+//     name: 'Daniil',
+//     age: 21
+// }
+
+// person.logInfo()
+// // person.logInfo.bind(Danya)('Programmist', '123')
+// const fnDanyaInfoLog = person.logInfo.bind(Danya) //, 'Web-designer', '+79027******'\
+// fnDanyaInfoLog('Web-designer', '+79027******')
+
+// person.logInfo.call(Danya, 'Web-designer', '+79027******')
+// person.logInfo.apply(Danya, ['Web-designer', '+79027******']) //Можно передавать только 2 параметра
+
+// //==========
+
+// const array = [1, 2, 3, 4, 5]
+
+// // function multBy(arr, n) {
+// // return arr.map(function(i){
+// //     return i * n
+// // })
+// // }
+
+// // console.log(multBy(array, 15))
 
 
-Array.prototype.multBy = function(n){
-    return this.map(function(i){
-             return i * n
-         })
+// Array.prototype.multBy = function(n){
+//     return this.map(function(i){
+//              return i * n
+//          })
+// }
+
+// console.log(array.multBy(3))
+
+function createCalFunction(n) {
+    return function() {
+        console.log(1000 * n)
+    }
 }
 
-console.log(array.multBy(3))
+const  calc = createCalFunction(42)
+// console.log(calc)
+calc()
 
+function createIncrementor(m) {
+    return function(num) {
+        return m + num
+    } 
+}
+
+const addOne = createIncrementor(1)
+const addTen = createIncrementor(10)
+
+console.log(addOne(10))
+console.log(addOne(45))
+
+console.log(addTen(10))
+console.log(addTen(45))
+
+function urlGenerator(domain) {
+    return function(url) {
+        return `https://${url}.${domain}`
+    }
+}
+
+const comUrl = urlGenerator('com')
+const ruUrl = urlGenerator('ru')
+
+console.log(comUrl('google'))
+console.log(ruUrl('twitch'))
+
+
+
+function bind(context, fn) {
+    return function(...args) {
+        fn.apply(context, args)
+    }
+}
+
+function logPerson() {
+    console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+
+const person1 = {name: 'Даниил', age: 20, job: 'Безработный'}
+const person2 = {name: 'Игорь', age: 24, job: 'Продавец'}
+
+bind(person1, logPerson)()
+bind(person2, logPerson)()
