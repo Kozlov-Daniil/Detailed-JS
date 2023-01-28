bc7cca3bd9492c96dc1368bcb292a87fff6bc03b
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2023-01-28 20:10:48 +0300

    Fifth lesson, what is a "Promise"

--- index.js
+++ index.js
@@ -134,21 +134,94 @@
 // bind(person1, logPerson)()
 // bind(person2, logPerson)()
 
-console.log('Start')
-console.log('Start 2')
+// console.log('Start')
+// console.log('Start 2')
 
-function timeout2sec() {
-    console.log('timeout2sec')
+// function timeout2sec() {
+//     console.log('timeout2sec')
+// }
+
+// setTimeout(function() {
+//     console.log('Inside timeout, after 0 seconds')
+// }, 0)
+
+// window.setTimeout(function() {
+//     console.log('Inside timeout, after 5 seconds')
+// }, 5000)
+
+// setTimeout(timeout2sec, 2000)
+
+// console.log('End')
+
+console.log('Request data...')
+
+// setTimeout(() => {
+//   console.log('Preparing data...')
+
+//   const backendData = {
+//     server: 'aws',
+//     port: 2000,
+//     status: 'working'
+//   }
+
+//   setTimeout(() => {
+//     backendData.modified = true
+//     console.log('Data recieved', backendData)
+//   }, 2000)
+// }, 2000)
+
+const p = new Promise(function(resolve, reject) {  // 2 аргумента в функции 
+    setTimeout(() => {
+        console.log('Preparing data...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+          }
+        resolve(backendData)
+    }, 2000)
+})
+
+p.then(data => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true
+            resolve(data)  // Если reject - выведет ошибку
+            // console.log('Data recieved', backendData)
+          }, 2000)
+    })
+
+    // p2.then(clientData => {
+    //     console.log('Data recieved', clientData)
+    // })
+})
+.then(clientData => {
+    console.log('Data recieved', clientData)
+    clientData.fromPromise = true
+    return clientData 
+}).then(data => {
+    console.log('Modified', data)
+})
+.catch(err => console.error('Error: ', err)) // Выведение ошибки в случае с reject
+.finally(() => console.log('Finally'))
+
+/////
+
+const sleep = ms => {
+   return new Promise(resolve => {
+    setTimeout(() => resolve(), ms)
+   })
 }
 
-setTimeout(function() {
-    console.log('Inside timeout, after 0 seconds')
-}, 0)
+// sleep(2000).then(() => console.log('After 2 sec'))
+// sleep(3000).then(() => console.log('After 3 sec'))
 
-window.setTimeout(function() {
-    console.log('Inside timeout, after 5 seconds')
-}, 5000)
+Promise.all([sleep(2000), sleep(5000)])
+.then(() => {
+    console.log('All promises')
+})
 
-setTimeout(timeout2sec, 2000)
-
-console.log('End')
+Promise.race([sleep(2000), sleep(5000)])
+.then(() => {
+    console.log('Race promises')
+})
