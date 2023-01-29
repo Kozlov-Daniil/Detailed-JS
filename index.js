f4eefcbffbb4e7f7a7d1ef397a327bbb52bc55ac
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2023-01-29 23:19:03 +0300

    Eighth lesson, what is a 'async', 'await', 'fetch'

--- index.js
+++ index.js
@@ -269,102 +269,138 @@
 //     hasTail: true
 // }
 
-class Animal{
+// class Animal{
 
-    static type = 'ANIMAL'
+//     static type = 'ANIMAL'
 
-    constructor(options){
-        this.name = options.name
-        this.age = options.age
-        this.hasTail = options.hasTail
-    }
+//     constructor(options){
+//         this.name = options.name
+//         this.age = options.age
+//         this.hasTail = options.hasTail
+//     }
 
-    voice() {
-        console.log('I am animal')
+//     voice() {
+//         console.log('I am animal')
+//     }
+// }
+
+// const animal = new Animal({
+//     name: 'Animal',
+//     age: 5,
+//     hasTail: true
+// })
+
+// class Cat extends Animal {
+//     static type = 'CAT'
+
+//     constructor(options){
+//         super(options)  //Вызов родительского конструктора
+//         this.color = options.color
+//     }
+
+//     voice() {
+//         super.voice()
+//         console.log('I am cat')
+//     }
+
+//     get ageInfo() {
+//         return this.age * 7
+//     }
+
+//     set ageInfo(newAge) {
+//         this.age = newAge
+//     }
+// }
+
+// const cat = new Cat({
+//     name: 'Cat',
+//     age: 7,
+//     hasTail: true,
+//     color: 'black'
+// })
+
+
+// /////
+
+// class Component {
+//     constructor(selector) {
+//         this.$el = document.querySelector(selector)
+//     }
+
+//     hide() {
+//         this.$el.style.display = 'none'
+//     }
+
+//     show() {
+//         this.$el.style.display = 'block'
+//     }
+// }
+
+// class Box extends Component{
+//     constructor(options) {
+//         super(options.selector)
+//         this.$el.style.width = this.$el.style.height = options.size + 'px'
+//         this.$el.style.background = options.color
+//     }
+// }
+
+// const box1 = new Box({
+//     selector: '#box1',
+//     size: 100,
+//     color: 'red'
+// })
+
+// const box2 = new Box({
+//     selector: '#box2',
+//     size: 150,
+//     color: 'black'
+// })
+
+// class Circle extends Box{
+//     constructor(options) {
+//         super(options)
+//         this.$el.style.borderRadius = '50%'
+//     }
+// }
+
+// const c = new Circle ({
+//     selector: '#circle',
+//     size: 90,
+//     color: 'blue'
+// })
+
+const delay = ms => {
+    return new Promise (r => setTimeout(() => r(), ms))
+}
+
+delay(2000).then(() => console.log('2 sec'))
+
+const url = 'https://jsonplaceholder.typicode.com/todos'
+
+// function fetchTodos() {
+//     console.log('Fetch todo started...')
+//     return delay(3000).then(() => {
+//       return fetch(url)
+//     }).then(response => response.json())
+// }
+
+// fetchTodos().then(data => {
+//     console.log('Data:', data)
+// })
+// .catch(e => console.error(e))
+
+async function fetchAsuncTodos() {
+    console.log('Fetch todo started...')
+    try {
+        await delay(3000)
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log('Data:', data)
+    } catch(e) {
+        console.error(e)
+    } finally{
+        
     }
 }
 
-const animal = new Animal({
-    name: 'Animal',
-    age: 5,
-    hasTail: true
-})
-
-class Cat extends Animal {
-    static type = 'CAT'
-
-    constructor(options){
-        super(options)  //Вызов родительского конструктора
-        this.color = options.color
-    }
-
-    voice() {
-        super.voice()
-        console.log('I am cat')
-    }
-
-    get ageInfo() {
-        return this.age * 7
-    }
-
-    set ageInfo(newAge) {
-        this.age = newAge
-    }
-}
-
-const cat = new Cat({
-    name: 'Cat',
-    age: 7,
-    hasTail: true,
-    color: 'black'
-})
-
-
-/////
-
-class Component {
-    constructor(selector) {
-        this.$el = document.querySelector(selector)
-    }
-
-    hide() {
-        this.$el.style.display = 'none'
-    }
-
-    show() {
-        this.$el.style.display = 'block'
-    }
-}
-
-class Box extends Component{
-    constructor(options) {
-        super(options.selector)
-        this.$el.style.width = this.$el.style.height = options.size + 'px'
-        this.$el.style.background = options.color
-    }
-}
-
-const box1 = new Box({
-    selector: '#box1',
-    size: 100,
-    color: 'red'
-})
-
-const box2 = new Box({
-    selector: '#box2',
-    size: 150,
-    color: 'black'
-})
-
-class Circle extends Box{
-    constructor(options) {
-        super(options)
-        this.$el.style.borderRadius = '50%'
-    }
-}
-
-const c = new Circle ({
-    selector: '#circle',
-    size: 90,
-    color: 'blue'
-})
+fetchAsuncTodos()
