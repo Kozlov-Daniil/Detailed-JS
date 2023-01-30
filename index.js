3c82f2105eab30f35b8e5edd905c116d1252f8ca
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2023-01-30 13:06:36 +0300

    Ninth lesson, what is a "proxy"
    What is a "proxy (objects, functions, classes)

--- index.js
+++ index.js
@@ -369,38 +369,107 @@
 //     color: 'blue'
 // })
 
-const delay = ms => {
-    return new Promise (r => setTimeout(() => r(), ms))
-}
-
-delay(2000).then(() => console.log('2 sec'))
-
-const url = 'https://jsonplaceholder.typicode.com/todos'
-
-// function fetchTodos() {
-//     console.log('Fetch todo started...')
-//     return delay(3000).then(() => {
-//       return fetch(url)
-//     }).then(response => response.json())
+// const delay = ms => {
+//     return new Promise (r => setTimeout(() => r(), ms))
 // }
 
-// fetchTodos().then(data => {
-//     console.log('Data:', data)
-// })
-// .catch(e => console.error(e))
+// delay(2000).then(() => console.log('2 sec'))
 
-async function fetchAsuncTodos() {
-    console.log('Fetch todo started...')
-    try {
-        await delay(3000)
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log('Data:', data)
-    } catch(e) {
-        console.error(e)
-    } finally{
+// const url = 'https://jsonplaceholder.typicode.com/todos'
+
+// // function fetchTodos() {
+// //     console.log('Fetch todo started...')
+// //     return delay(3000).then(() => {
+// //       return fetch(url)
+// //     }).then(response => response.json())
+// // }
+
+// // fetchTodos().then(data => {
+// //     console.log('Data:', data)
+// // })
+// // .catch(e => console.error(e))
+
+// async function fetchAsuncTodos() {
+//     console.log('Fetch todo started...')
+//     try {
+//         await delay(3000)
+//         const response = await fetch(url)
+//         const data = await response.json()
+//         console.log('Data:', data)
+//     } catch(e) {
+//         console.error(e)
+//     } finally{
         
+//     }
+// }
+
+// fetchAsuncTodos()
+
+
+
+//Objects
+const person = {
+    name: 'Eagle',
+    age: 20,
+    job: 'Frontend'
+}
+
+const op = new Proxy(person, {
+    get(target, prop) {
+        console.log('Target', target)
+        console.log('Prop', prop)
+        console.log(`Getting prop ${prop}`)
+        if(!(prop in target)) {
+            return prop.split('_').map(p => target[p]).join(' ')
+        }
+        return target[prop]
+    },
+    set(target, prop, value) {
+        if (prop in target){
+            target[prop] = value
+        } else {
+            throw new Error(`No ${prop} field in target`)
+        }      
+    },
+    has(target, prop) {
+        return ['age', 'name', 'job'].includes(prop)
+    },
+    deleteProperty(target, prop) {
+        console.log('Deleting', prop)
+        delete target[prop]
+        return true
+    }
+})
+
+//Functions 
+const log = text => `Log: ${text}`
+
+const fp = new Proxy(log, {
+    apply(target, thisArg, args) {  //Принимает 3 параметра
+        console.log('Calling fn...')
+        return target.apply(thisArg, args).toUpperCase()
+    }
+})
+
+//Classes
+
+class Person {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
     }
 }
 
-fetchAsuncTodos()
+const PersonProxy = new Proxy(Person, {
+    construct(target, args) {
+        console.log('Construct...')
+        return new Proxy (new target(...args), {
+            get(t, prop) {
+                console.log(`Getting prop "${prop}"`)
+                return t[prop]
+            }
+        })
+    }
+})
+
+const p = new PersonProxy('Eagle', 20)
