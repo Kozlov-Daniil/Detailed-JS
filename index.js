e16781175a88dd996479f80c0ba0e0ac62734f7a
Kozlov-Daniil <57208983+Kozlov-Daniil@users.noreply.github.com>
2023-02-01 02:03:01 +0300

    Eleventh lesson, what is a 'generator'
    What is a 'generator', 'symbol iterator', 'for of'

--- index.js
+++ index.js
@@ -478,66 +478,110 @@
 
 // Wrapper
 
-const withDefaultValue = (target, defaultValue = 0) => {
-    return new Proxy(target, {
-        get: (obj, prop) => (prop in obj ? obj[prop]: defaultValue)
-    })
+// const withDefaultValue = (target, defaultValue = 0) => {
+//     return new Proxy(target, {
+//         get: (obj, prop) => (prop in obj ? obj[prop]: defaultValue)
+//     })
+// }
+
+// const position = withDefaultValue({
+//     x: 24,
+//     y: 42
+// },0
+// )
+
+// console.log(position)
+
+// //Hidden properties
+
+// const withHiddenProps = (target, prefix = '_') => {
+//     return new Proxy(target, {
+//         has: (obj, prop) => (prop in obj) && (!prop.startsWith(prefix)),
+//         ownKeys: obj => Reflect.ownKeys(obj).filter(p => !p.startsWith(prefix)),
+//         get: (obj, prop, receiver) => (prop in receiver) ? obj[prop]: void 0
+//     })
+// }
+
+// const data = withHiddenProps({
+//     name: 'Eagle',
+//     age: 20,
+//     _uid: '123'
+// })
+
+
+// //Optimization
+
+
+// // const index = {}
+// // userData.forEach(i => (index[i.id] = i))
+
+// const IndexedArray = new Proxy(Array, {
+//     construct(target, [args]) {
+//         const index = {}
+//         args.forEach(item => (index[item.id] = item))
+
+//         return new Proxy (new target(...args), {
+//             get(arr, prop) {
+//                 switch (prop) {
+//                     case 'push': return item => {
+//                         index[item.id] = item
+//                         arr[prop].call(arr, item)
+//                     }
+//                     case 'findById': return id => index[id]
+//                     default: return arr[prop]
+//                 }
+//             }
+//         })
+//     }
+// })
+
+// const users = new IndexedArray([
+//     {id: 1, name: 'Eagle', job: 'Frontend', age: 20},
+//     {id: 2, name: 'Mihail', job: 'Fullstack', age: 21},
+//     {id: 3, name: 'Daniil', job: 'Backend', age: 22},
+//     {id: 4, name: 'Lisa', job: 'Teacher', age: 23}
+// ])
+
+
+function* strGenerator() {
+    yield 'H'
+    yield 'e'
+    yield 'l'
+    yield 'l'
+    yield 'o'
 }
 
-const position = withDefaultValue({
-    x: 24,
-    y: 42
-},0
-)
+const str = strGenerator()
 
-console.log(position)
-
-//Hidden properties
-
-const withHiddenProps = (target, prefix = '_') => {
-    return new Proxy(target, {
-        has: (obj, prop) => (prop in obj) && (!prop.startsWith(prefix)),
-        ownKeys: obj => Reflect.ownKeys(obj).filter(p => !p.startsWith(prefix)),
-        get: (obj, prop, receiver) => (prop in receiver) ? obj[prop]: void 0
-    })
-}
-
-const data = withHiddenProps({
-    name: 'Eagle',
-    age: 20,
-    _uid: '123'
-})
-
-
-//Optimization
-
-
-// const index = {}
-// userData.forEach(i => (index[i.id] = i))
-
-const IndexedArray = new Proxy(Array, {
-    construct(target, [args]) {
-        const index = {}
-        args.forEach(item => (index[item.id] = item))
-
-        return new Proxy (new target(...args), {
-            get(arr, prop) {
-                switch (prop) {
-                    case 'push': return item => {
-                        index[item.id] = item
-                        arr[prop].call(arr, item)
-                    }
-                    case 'findById': return id => index[id]
-                    default: return arr[prop]
-                }
-            }
-        })
+function* numberGen(n = 10) {
+    for(let i = 0; i < n; i++) {
+        yield i
     }
-})
+}
 
-const users = new IndexedArray([
-    {id: 1, name: 'Eagle', job: 'Frontend', age: 20},
-    {id: 2, name: 'Mihail', job: 'Fullstack', age: 21},
-    {id: 3, name: 'Daniil', job: 'Backend', age: 22},
-    {id: 4, name: 'Lisa', job: 'Teacher', age: 23}
-])
+const num = numberGen(7)
+
+const iterator = {
+    [Symbol.iterator](w = 10) {
+        let q = 0
+        return {
+            next() {
+                if (q < w) {
+                    return {value: ++q, done: false}
+                }
+                return {value: undefined, done: true}
+            }
+        }
+    }
+}
+
+function* iter(x = 10) {
+    for (let c = 0; c < x; c++) {
+        yield c
+    }
+}
+
+
+for(let z of iter(5)) {
+    console.log(z)
+}
